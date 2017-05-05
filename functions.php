edf75eefe4227ad412f202d0557698c56eae9656
lilihaas <lhaas001@seattlecentral.edu>
2017-05-04 23:29:55 -0700

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+     
+/*
+Theme Name: Web170
+Author:Liliane Haas
+Author URI: http://lilihaas.com/
+Description: This is the demo theme for the WEB170 class for the Spring 2017
+Version:42.0
+*/
+     
+
+
+//Register Navigation Menus (it wll show the "menus" in the dashboard just like in the others themes)
+register_nav_menus(array('main-menu' => __('Main Menu')));
+
+?>
